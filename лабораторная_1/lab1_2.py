d333cfcb5efaea31e1d534a954186cfa8bc3e3c0
5ea6uck7horn <alexivaxxx@gmail.com>
2024-10-30 01:28:00 +0300

    лабораторная_1

---
+++ лабораторная_1/lab1_2.py
@@ -0,0 +1,12 @@
+# TODO Найдите количество книг, которое можно разместить на дискете
+disk_volume_mb = 1.44 #мб
+pages_per_book = 100
+lines_per_page = 50
+symbols_per_line = 25
+symbol_volume = 4 #б
+
+disk_volume_b = 1.44 * 1024 * 1024
+symbols_per_book = symbols_per_line * lines_per_page * pages_per_book
+book_volume = symbols_per_book * symbol_volume
+books_per_disk = int(disk_volume_b / book_volume)
+print("Количество книг, помещающихся на дискету:", books_per_disk)
