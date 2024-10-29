26cf3347e75afc0184d07a26954164a2b322b813
5ea6uck7horn <alexivaxxx@gmail.com>
2024-10-30 01:29:26 +0300

    лабораторная_2

---
+++ лабораторная_2/lab2_2.py
@@ -0,0 +1,10 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+minimal_needed_budget = 0
+for i in range(months):
+    increased_spend = spend * ((1 + increase) ** i)
+    minimal_needed_budget += (increased_spend - salary)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {minimal_needed_budget:.0f}")
