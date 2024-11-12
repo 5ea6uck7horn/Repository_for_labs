a4853ed5e8a9907056bfecb0ca7f22e050d5b5c2
5ea6uck7horn <alexivaxxx@gmail.com>
2024-11-13 00:04:03 +0300

    лабораторная_4

---
+++ лабораторная_4/lab4_1.py
@@ -0,0 +1,12 @@
+import json
+
+
+def task(filename: str) -> float:
+    i = 0
+    with open(filename) as file:
+        data = json.load(file)
+    summed_values = sum([item["score"] * item["weight"] for item in data])
+    return round(summed_values, 3)
+
+
+print(task('input.json'))
