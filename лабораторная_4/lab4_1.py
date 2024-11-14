c2c24fc50fd97408c30ff938ce0d5502975e7d00
5ea6uck7horn <alexivaxxx@gmail.com>
2024-11-14 20:54:16 +0300

    Update lab4_1.py

--- лабораторная_4/lab4_1.py
+++ лабораторная_4/lab4_1.py
@@ -1,7 +1,6 @@
 import json
 
 def task(filename: str) -> float:
-    i = 0
     with open(filename) as file:
         data = json.load(file)
     summed_values = sum([item["score"] * item["weight"] for item in data])
