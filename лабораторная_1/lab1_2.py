81d1b92a2bb28932b21de9596a90e7a73a554f7a
5ea6uck7horn <alexivaxxx@gmail.com>
2024-11-24 18:46:55 +0300

    Update lab1_2.py

--- лабораторная_1/lab1_2.py
+++ лабораторная_1/lab1_2.py
@@ -1,8 +1,8 @@
-disk_volume_mb = 1.44 #мб
+disk_volume_mb = 1.44 # MB
 pages_per_book = 100
 lines_per_page = 50
 symbols_per_line = 25
-symbol_volume = 4 #б
+symbol_volume = 4 # bytes
 
 disk_volume_b = 1.44 * 1024 * 1024
 symbols_per_book = symbols_per_line * lines_per_page * pages_per_book
